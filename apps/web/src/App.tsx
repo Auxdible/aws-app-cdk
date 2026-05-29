849c08bf099aee27a3340ea38baa6b071987193a
Auxdible <stevenhotrod06@gmail.com>
2026-05-29 15:03:39 -0400

    fix: wrong env name

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
-          <p>Your api url is {import.meta.env.VITE_PUBLIC_API_URL}</p>
+          <p>Your api url is {import.meta.env.VITE_API_URL}</p>
         </div>
         <button
           type="button"
