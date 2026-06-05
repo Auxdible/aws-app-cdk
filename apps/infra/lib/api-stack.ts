da10d263eda3d260d487b7e9bff3f2dde0df49d2
Auxdible <stevenhotrod06@gmail.com>
2026-06-05 07:20:10 -0400

    feat: add some dynamodb things

--- apps/infra/lib/api-stack.ts
+++ apps/infra/lib/api-stack.ts
@@ -4,16 +4,27 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as api_gateway from "aws-cdk-lib/aws-apigatewayv2";
 import * as integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
 import * as path from "node:path";
-
+import * as dynamoDB from "aws-cdk-lib/aws-dynamodb";
 export class ApiStack extends cdk.Stack {
   public readonly apiUrl: string;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const myTable = new dynamoDB.TableV2(this, "MyTable", {
+      partitionKey: { name: "pk", type: dynamoDB.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     const healthLambda = new lambda.Function(this, "HealthLambda", {
       code: lambda.Code.fromAsset(path.join(__dirname, "lambda/health")),
       handler: "index.health_handler",
       runtime: lambda.Runtime.PYTHON_3_14,
+      environment: {
+        TABLE_NAME: myTable.tableName,
+      },
     });
+    myTable.grantFullAccess(healthLambda);
+
     const healthIntegration = new integrations.HttpLambdaIntegration(
       "HealthIntegration",
       healthLambda,
--- apps/infra/lib/lambda/health/index.py
+++ apps/infra/lib/lambda/health/index.py
@@ -1,8 +1,25 @@
 import boto3
+import os
 import botocore
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
 
 def health_handler(event, context):
+    tableName = os.environ.get("TABLE_NAME");
+    if tableName is None:
+        return {
+            "statusCode": 500,
+            "body": "You are missing a table name environment variable!"
+        }
+
+    dynamodb.put_item(TableName=os.environ.get('TABLE_NAME'), Item={
+        "pk": uuid.uuid7(),
+        "ip": event['requestContext']['http']['sourceIp'],
+        "message": "Test of me!!! DO I WORK!!!"
+    });  
+   
     return {
         "statusCode": 200,
-        "body": "health check received!"
+        "body": "health check received and posted a thing to the table"
     }
